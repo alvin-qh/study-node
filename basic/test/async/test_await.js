28ba6eada887e82b139bc92dcea62bf1aa960281
Alvin <mousebaby8080@gmail.com>
2017-10-28 22:49:10 +0800

    fix test name

--- basic/test/async/test_await.js
+++ basic/test/async/test_await.js
@@ -1,13 +1,13 @@
 import {expect} from "chai";
 import {after, throwErrorThisTime} from "../../src/async/await";
 
-describe('Test promise', () => {
-	it("Test promise successful", async () => {
+describe('Test await', () => {
+	it("Test await successful", async () => {
 		const msg = await after(100);
 		expect(msg).is.eql('OK');
 	});
 
-	it("Test promise error", async () => {
+	it("Test await error", async () => {
 		throwErrorThisTime();
 
 		try {
