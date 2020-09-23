e3f97e07b2579c47f1d695a7e871dfcf016c7e29
quhao <quhao@wisdomgarden.com>
2020-09-23 13:19:24 +0800

    [all] upgrade package

--- express/asset/js/common/ns.js
+++ express/asset/js/common/ns.js
@@ -1,14 +1,14 @@
 const global = global || window;
 
 export default function (name, obj) {
-    const parts = name.split('.');
+  const parts = name.split('.');
 
-    let ns = global;
-    for (let i = 0; i < parts.length - 1; i++) {
-        const part = parts[i];
+  let ns = global;
+  for (let i = 0; i < parts.length - 1; i++) {
+    const part = parts[i];
 
-        ns[part] = ns[part] || {};
-        ns = ns[part];
-    }
-    ns[parts[parts.length - 1]] = obj;
+    ns[part] = ns[part] || {};
+    ns = ns[part];
+  }
+  ns[parts[parts.length - 1]] = obj;
 };
--- express/webpack.config.js
+++ express/webpack.config.js
@@ -7,191 +7,186 @@ const CleanupPlugin = require('webpack-cleanup-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WebpackAssetsPlugin = require('webpack-assets-manifest');
 
-
 function normalizePath(p) {
-    return p.replace(/\\/g, '/');
+  return p.replace(/\\/g, '/');
 }
 
 const CONFIG = {
-    isProd: process.env.NODE_ENV === 'production',
-    paths: {
-        src: file => normalizePath(path.join('asset', file || '')),
-        dest: file => normalizePath(path.join('src/public', file || ''))
-    }
+  isProd: process.env.NODE_ENV === 'production',
+  paths: {
+    src: file => normalizePath(path.join('asset', file || '')),
+    dest: file => normalizePath(path.join('src/public', file || ''))
+  }
 };
 
 function makeEntries() {
-    const src = `./${CONFIG.paths.src('js')}/`;
-    const entries = {};
+  const src = `./${CONFIG.paths.src('js')}/`;
+  const entries = {};
 
-    glob.sync(path.join(src, '/**/main.js')).map(file => `./${file}`)
-        .forEach(file => {
-            let name = normalizePath(path.dirname(file));
-            name = name.substr(name.lastIndexOf('/') + 1);
-            entries[name] = file;
-        });
-    return entries;
+  glob.sync(path.join(src, '/**/main.js')).map(file => `./${file}`)
+    .forEach(file => {
+      let name = normalizePath(path.dirname(file));
+      name = name.substr(name.lastIndexOf('/') + 1);
+      entries[name] = file;
+    });
+  return entries;
 }
 
 const plugins = (() => {
-    const ProvidePlugin = webpack.ProvidePlugin;
-    const ProgressPlugin = webpack.ProgressPlugin;
+  const ProvidePlugin = webpack.ProvidePlugin;
+  const ProgressPlugin = webpack.ProgressPlugin;
 
-    let plugins = [
-        new ProgressPlugin(),
-        new CleanupPlugin({
-            quiet: !CONFIG.isProd,
-            exclude: CONFIG.isProd ? [] : ['fonts/**/*', 'images/**/*']
-        }),
-        new ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: CONFIG.paths.src('images/*'),
-                    to: 'images/[name].[ext]'
-                }
-            ]
-        }),
-        new MiniCssExtractPlugin({
-            filename: CONFIG.isProd ? 'css/[name]-[chunkhash:8].css' : 'css/[name].css'
-        })
-    ];
+  let plugins = [
+    new ProgressPlugin(),
+    new CleanupPlugin({
+      quiet: !CONFIG.isProd,
+      exclude: CONFIG.isProd ? [] : ['fonts/**/*', 'images/**/*']
+    }),
+    new ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: CONFIG.paths.src('images/*'),
+          to: 'images/[name].[ext]'
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: CONFIG.isProd ? 'css/[name]-[chunkhash:8].css' : 'css/[name].css'
+    })
+  ];
 
-    if (CONFIG.isProd) {
-        plugins = plugins.concat([
-            new WebpackAssetsPlugin({
-                output: 'manifest.json',
-                merge: false,
-                customize(key, value, originalValue, manifest) {
-                    switch (manifest.getExtension(value).substr(1).toLowerCase()) {
-                        case 'js.map':
-                        case 'css.map':
-                            return false;
-                        case 'js':
-                            key = `js/${key}`;
-                            break;
-                        case 'css':
-                            key = `css/${key}`;
-                            break;
-                    }
-                    return {
-                        key: key,
-                        value: value
-                    }
-                }
-            })
-        ]);
-    }
-    return plugins;
+  if (CONFIG.isProd) {
+    plugins = plugins.concat([
+      new WebpackAssetsPlugin({
+        output: 'manifest.json',
+        merge: false,
+        customize(key, value, originalValue, manifest) {
+          switch (manifest.getExtension(value).substr(1).toLowerCase()) {
+            case 'js.map':
+            case 'css.map':
+              return false;
+            case 'js':
+              key = `js/${key}`;
+              break;
+            case 'css':
+              key = `css/${key}`;
+              break;
+          }
+          return {
+            key: key,
+            value: value
+          }
+        }
+      })
+    ]);
+  }
+  return plugins;
 })();
 
 module.exports = {
-    mode: CONFIG.isProd ? 'production' : 'development',
-    entry: Object.assign(
+  mode: CONFIG.isProd ? 'production' : 'development',
+  entry: Object.assign(
+    {
+      vendor: ['jquery', 'bootstrap', 'moment', 'lodash', 'common']
+    },
+    makeEntries()
+  ),
+  output: {
+    path: path.resolve(CONFIG.paths.dest()),
+    filename: CONFIG.isProd ? 'js/[name]-[chunkhash:8].js' : 'js/[name].js',
+    publicPath: '/',
+    chunkFilename: CONFIG.isProd ? 'js/[name]-[chunkhash:8].js' : 'js/[name].js',
+  },
+  resolve: {
+    alias: {
+      common: `./${CONFIG.paths.src('js')}/common/common.js`,
+    },
+    extensions: ['.js', '.vue', '.json']
+  },
+  optimization: {
+    minimize: CONFIG.isProd,
+    removeEmptyChunks: true,
+    runtimeChunk: {
+      name: 'manifest',
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: [/node_modules/],
+      use: [
         {
-            vendor: ['jquery', 'bootstrap', 'moment', 'lodash', 'common']
-        },
-        makeEntries()
-    ),
-    output: {
-        path: path.resolve(CONFIG.paths.dest()),
-        filename: CONFIG.isProd ? 'js/[name]-[chunkhash:8].js' : 'js/[name].js',
-        publicPath: '/',
-        chunkFilename: CONFIG.isProd ? 'js/[name]-[chunkhash:8].js' : 'js/[name].js',
-    },
-    resolve: {
-        alias: {
-            common: `./${CONFIG.paths.src('js')}/common/common.js`,
-        },
-        extensions: ['.js', '.vue', '.json']
-    },
-    optimization: {
-        minimize: CONFIG.isProd,
-        removeEmptyChunks: true,
-        // splitChunks: {
-        //     chunks: 'all',
-        //     name: 'global'
-        // },
-        runtimeChunk: {
-            name: 'manifest',
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env']
+          }
         }
+      ]
     },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: [/node_modules/],
-            use: [
-                {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/env']
-                    }
-                }
-            ]
+    {
+      test: /\.(less|css)$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader
         },
         {
-            test: /\.(less|css)$/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                },
-                {
-                    loader: 'css-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                },
-                {
-                    loader: 'less-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                }
-            ]
-        }, {
-            test: /\.(eot|woff|woff2|ttf)$/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    query: {
-                        limit: 10240,
-                        name: CONFIG.isProd ? 'fonts/[name]-[hash:8].[ext]' : 'fonts/[name].[ext]'
-                    }
-                },
-                {
-                    loader: 'url-loader',
-                    query: {
-                        limit: 10240,
-                        fallback: 'file-loader',
-                        name: CONFIG.isProd ? 'fonts/[name]-[hash:8].[ext]' : 'fonts/[name].[ext]'
-                    }
-                }
-            ]
-        }, {
-            test: /\.(svg|png|jpg|gif)$/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    query: {
-                        limit: 10240,
-                        name: CONFIG.isProd ? 'images/[name]-[hash:8].[ext]' : 'images/[name].[ext]'
-                    }
-                },
-                {
-                    loader: 'url-loader',
-                    query: {
-                        limit: 10240,
-                        fallback: 'file-loader',
-                        name: CONFIG.isProd ? 'images/[name]-[hash:8].[ext]' : 'images/[name].[ext]'
-                    }
-                }
-            ]
-        }]
-    },
-    plugins: plugins,
-    devtool: 'cheap-source-map',
+          loader: 'css-loader',
+          options: {
+            sourceMap: true
+          }
+        },
+        {
+          loader: 'less-loader',
+          options: {
+            sourceMap: true
+          }
+        }
+      ]
+    }, {
+      test: /\.(eot|woff|woff2|ttf)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            limit: 10240,
+            name: CONFIG.isProd ? 'fonts/[name]-[hash:8].[ext]' : 'fonts/[name].[ext]'
+          }
+        },
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 10240,
+            fallback: 'file-loader',
+            name: CONFIG.isProd ? 'fonts/[name]-[hash:8].[ext]' : 'fonts/[name].[ext]'
+          }
+        }
+      ]
+    }, {
+      test: /\.(svg|png|jpg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            limit: 10240,
+            name: CONFIG.isProd ? 'images/[name]-[hash:8].[ext]' : 'images/[name].[ext]'
+          }
+        },
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 10240,
+            fallback: 'file-loader',
+            name: CONFIG.isProd ? 'images/[name]-[hash:8].[ext]' : 'images/[name].[ext]'
+          }
+        }
+      ]
+    }]
+  },
+  plugins: plugins,
+  devtool: 'cheap-source-map',
 };
