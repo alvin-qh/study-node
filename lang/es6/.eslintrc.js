cee6a86eb33866d2acbbf374ff86707c4c5cce30
瞿昊 <quhao@handaotech.com>
2023-08-29 14:41:04 +0800

    [es6] fix code

---
+++ lang/es6/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "env": {
+    "es2021": true,
+    "node": true,
+    "mocha": true
+  },
+  "extends": "eslint:recommended",
+  "overrides": [
+    {
+      "files": [
+        "src/**/*.js",
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      },
+      "rules": {
+        "simple-import-sort/imports": "off"
+      }
+    }
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "simple-import-sort"
+  ],
+  "rules": {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "linebreak-style": ["error", "unix"],
+    "indent": ["warn", 2, { "ignoredNodes": ["PropertyDefinition"] }],
+    "quotes": ["warn", "double"],
+    "semi": ["error", "always"],
+    "sort-imports": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  }
+}
