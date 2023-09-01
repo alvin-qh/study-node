cd11ea90345460158ed539a91d816fbd41320c04
瞿昊 <quhao@handaotech.com>
2023-09-01 16:03:13 +0800

    [koa] add koa demo

---
+++ web/koa/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true,
+    "mocha": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "overrides": [
+    {
+      "files": [
+        "src/**/*.ts",
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      },
+      "rules": {
+        "simple-import-sort/imports": "off"
+      }
+    }
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "simple-import-sort"
+  ],
+  "rules": {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "linebreak-style": ["error", "unix"],
+    "indent": ["warn", 2, { "ignoredNodes": ["PropertyDefinition"] }],
+    "quotes": ["warn", "double"],
+    "semi": ["error", "always"],
+    "sort-imports": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  }
+};
