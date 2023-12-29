a7ff2239ef2dc2cbd7b1b7734b85ef95eee6f67a
瞿昊 <quhao317@163.com>
2023-12-29 19:22:51 +0800

    [thirdpart/dotenv] add demo

--- thirdpart/dotenv/src/config.spec.ts
+++ thirdpart/dotenv/src/config.spec.ts
@@ -3,21 +3,24 @@ import { expect } from 'chai';
 import { loadEnvVariables } from './config';
 
 describe('Test dotenv library', () => {
+  beforeEach(() => {
+    delete process.env.APP_USER;
+    delete process.env.APP_VARIABLE;
+  });
+
   it('should load env variables from `.env` file', () => {
     loadEnvVariables();
 
-    expect(process.env.APP_USER).is.eq('prod-user');
-    expect(process.env.APP_VARIABLE).is.eq('Test production dotenv by prod-user');
+    expect(process.env.APP_USER).is.eq('dev-user');
+    expect(process.env.APP_VARIABLE).is.eq('Develope dotenv by dev-user');
   });
 
-  it('should load env variables from `.test.env` file', () => {
+  it('should load env variables from `.env.production` file', () => {
     loadEnvVariables({
-      path: '.test.env'
+      path: '.env.production'
     });
 
     expect(process.env.APP_USER).is.eq('prod-user');
-
-    expect(process.env.TEST_USER).is.eq('test-user');
-    expect(process.env.TEST_VARIABLE).is.eq('Test testing dotenv by test-user');
+    expect(process.env.APP_VARIABLE).is.eq('Production dotenv by prod-user');
   });
 });
