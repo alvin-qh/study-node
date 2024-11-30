b37603a27497d6ab56b1c5147e7abe00a3a8219b
Alvin <quhao317@163.com>
2024-12-01 01:12:30 +0800

    [lang/es6-commonjs] add es6 commonjs demo

---
+++ lang/es6-commonjs/eslint.config.mjs
@@ -0,0 +1,64 @@
+import globals from 'globals';
+import js from '@eslint/js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+        ...globals.chai,
+      },
+      sourceType: 'module',
+    },
+    rules: {
+      'comma-dangle': ['error', {
+        arrays: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+        imports: 'always-multiline',
+        objects: 'always-multiline',
+      }],
+      'import/no-extraneous-dependencies': 'off',
+      indent: ['warn', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      'no-multiple-empty-lines': ['warn', { max: 2, maxEOF: 0 }],
+      'no-param-reassign': 'off',
+      'no-plusplus': 'off',
+      'no-return-await': 'error',
+      'no-trailing-spaces': 'warn',
+      'no-underscore-dangle': 'off',
+      'object-curly-newline': ['error', {
+        ExportDeclaration: {
+          minProperties: 3,
+          multiline: true,
+        },
+        ImportDeclaration: 'never',
+        ObjectExpression: {
+          minProperties: 3,
+          multiline: true,
+        },
+        ObjectPattern: {
+          minProperties: 3,
+          multiline: true,
+        },
+      }],
+      'quote-props': ['error', 'as-needed'],
+      quotes: ['warn', 'single'],
+      semi: ['error', 'always'],
+      'sort-imports': ['warn', {
+        allowSeparatedGroups: true,
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      }],
+      'sort-keys': ['warn', 'asc', {
+        caseSensitive: true, minKeys: 2, natural: true,
+      }],
+    },
+  },
+];
