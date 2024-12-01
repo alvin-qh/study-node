6950593c0de0fe335d1ed983425acde10fc486f1
Alvin <quhao317@163.com>
2024-12-01 22:13:38 +0800

    [lang/ts] update typescript demo

--- lang/ts/src/index.ts
+++ lang/ts/src/index.ts
@@ -1,5 +1,8 @@
 import { add } from './module/misc';
 
+// 在 esModule 模式下, 如果需要通过 `tsc` 命令将 `ts` 脚本编译为 `js` 脚本, 则在 `import` 是需要加上 `.js` 扩展名, 否则编译后的结果无法正确执行 `import`
+// import { add } from './module/misc.js';
+
 function main(): void {
   const r = add(100, 200);
   console.log(`Hello Node.js, the result is: ${r}`);
--- lang/ts/src/module/misc.spec.ts
+++ lang/ts/src/module/misc.spec.ts
@@ -2,6 +2,9 @@ import { expect } from 'chai';
 
 import { add } from './misc';
 
+// 在 esModule 模式下, 如果需要通过 `tsc` 命令将 `ts` 脚本编译为 `js` 脚本, 则在 `import` 是需要加上 `.js` 扩展名, 否则编译后的结果无法正确执行 `import`
+// import { add } from './misc.js';
+
 /**
  * 测试 module 下面的 misc 模块
  */
