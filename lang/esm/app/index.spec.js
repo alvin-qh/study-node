c9e9448f48bfa05ff5dda46126ebdf94d2cf9d9c
Alvin <quhao317@163.com>
2024-12-07 21:23:56 +0800

    [lang/esm] update demo

--- lang/esm/app/index.spec.js
+++ lang/esm/app/index.spec.js
@@ -2,7 +2,13 @@ import { expect } from 'chai';
 
 import { add } from 'esm-lib';
 
+/**
+ * 测试导入 `esm-lib` 模块
+ */
 describe('test `es-module-lib`', () => {
+  /**
+   * 测试导入模块中的 `add` 函数工作正常
+   */
   it('should `add` function imported', () => {
     expect(add(1, 2)).to.equal(3);
   });
