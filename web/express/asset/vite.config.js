89a4b934e0183e9237e2a8e401f356e4fd2ddf34
AlvinQ <quhao317@163.com>
2024-12-12 22:37:08 +0800

    [web/express] update demo

---
+++ web/express/asset/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.ts'),
+      fileName: 'my-lib',
+      formats: ['cjs', 'es'],
+    },
+  }
+});
