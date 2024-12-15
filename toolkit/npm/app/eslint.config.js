1685d79f062e96391abba8788d7abe726ea07dc9
Alvin <quhao317@163.com>
2024-12-16 01:17:32 +0800

    [toolkit/npm] update demo

--- toolkit/npm/app/eslint.config.js
+++ toolkit/npm/app/eslint.config.js
@@ -60,7 +60,10 @@ export default [
           minProperties: 3,
           multiline: true,
         },
-        ImportDeclaration: 'never',
+        ImportDeclaration: {
+          minProperties: 5,
+          multiline: true,
+        },
         ObjectExpression: {
           minProperties: 3,
           multiline: true,
