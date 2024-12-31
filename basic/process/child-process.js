6186ce2beb42457469def533709c163c49ca0be4
AlvinQ <quhao317@163.com>
2024-12-31 19:10:48 +0800

    [basic] update demo

--- basic/process/child-process.js
+++ basic/process/child-process.js
@@ -1,11 +1,27 @@
-async function runChildProcess() {
-  return new Promise((resolve) => {
-    console.log('Hello Child process');
-    resolve();
+async function runChildProcess(name, data) {
+  return new Promise((resolve, reject) => {
+    try {
+      console.log(`child-process was started, 'Hello ${name}'`);
+
+      let n = 0;
+      const timer = setInterval(() => {
+        if (n >= data.length) {
+          clearInterval(timer);
+
+          process.send({ message: 'done' });
+          resolve();
+        } else {
+          process.send({ message: 'data', data: data[n] });
+          n++;
+        }
+      }, 50);
+    } catch (e) {
+      reject(e);
+    }
   });
 }
 
 if (process.env.CHILD === '1') {
-  const [name, age] = process.argv.slice(2);
-  await runChildProcess(name, age);
+  const [name, data] = process.argv.slice(2);
+  await runChildProcess(name, data.split(''));
 }
--- basic/process/fork.spec.js
+++ basic/process/fork.spec.js
@@ -8,31 +8,72 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 describe("test 'fork' function", () => {
   /**
+   * 通过 `fork` 方式启动子进程
    *
-   * @param {Array<string>} args 进程参数
+   * `fork` 方式相当于在当前 node 进程中分出一个子进程, 子进程和父进程共享内存空间
+   *
+   * @param {string} name 用户名
+   * @param {Array<string>} data 数据集合
    * @returns
    */
-  async function startChildProcess(args) {
+  async function startChildProcess(name, data) {
     return new Promise((resolve, reject) => {
-      const childProcess = fork(path.join(__dirname, './child-process.js'), args || [], {
+      const result = {
+        stdout: '',
+        code: 0,
+        signal: null,
+        data: [],
+      };
+
+      const ctrl = new AbortController();
+      const { signal } = ctrl;
+
+      const childProcess = fork(path.join(__dirname, './child-process.js'), [name, data], {
         env: { CHILD: '1' },
         silent: true,
+        signal,
       });
 
-      childProcess.stdout.on('data', data => resolve(data.toString()));
+      childProcess.stdout.on('data', data => {
+        result.stdout = data.toString();
+      });
+
+      childProcess.on('message', payload => {
+        if (payload.message === 'data') {
+          result.data.push(payload.data);
+        } else if (payload.message === 'done') {
+          // 如果子进程无法正常结束, 可采用如下方式结束子进程
+
+          // 通过信号量取消进程执行
+          // ctrl.abort('exit');
+
+          // 停止
+          // childProcess.kill();
+        }
+      });
 
       childProcess.on('error', reject);
 
       childProcess.on('exit', (code, signal) => {
-        if (code !== 0) {
-          reject(new Error(`Child process exited with code ${code} and signal ${signal}`));
+        if (code === 0) {
+          resolve({ ...result, code });
+        } else {
+          if (signal) {
+            reject(new Error(`child-process exit caused signal ${signal} and code is ${code}`));
+          } else {
+            reject(new Error(`child-process exit, code is ${code}`));
+          }
         }
       });
     });
   }
 
   it('should fork child-process from js file', async () => {
-    const result = await startChildProcess(['Alvin', '40']);
-    expect(result).to.equal('Hello Child process\n');
+    const result = await startChildProcess('Alvin', ['A', 'B', 'C', 'D']);
+
+    expect(result.stdout).to.equal("child-process was started, 'Hello Alvin'\n");
+    expect(result.code).to.equal(0);
+    expect(result.signal).is.null;
+    expect(result.data).to.deep.eq(['A', 'B', 'C', 'D']);
   });
 });
