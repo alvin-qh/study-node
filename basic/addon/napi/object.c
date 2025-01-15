29731b8afc0179e94c98b351dba6940ce8abda69
Alvin <quhao317@163.com>
2025-01-16 00:05:59 +0800

    [basic] update addon/napi demo

--- basic/addon/napi/object.c
+++ basic/addon/napi/object.c
@@ -1,3 +1,30 @@
+/// 通过 C 导出函数, 该函数可以返回一个 C 语言描述的 Node 函数
+///
+/// 下面 C 代码描述了如下 Node 代码
+///
+/// ```js
+/// export class Value {
+///   constructor(value) {
+///     this._value = value;
+///   }
+///
+///   get value() {
+///     return this._value;
+///   }
+///
+///   set value(value) {
+///     this.value = value;
+///   }
+///
+///   plusOne() {
+///     return this.value + 1;
+///   }
+///
+///   multiply(n) {
+///     return new Value(this.value * n);
+///   }
+/// }
+/// ```
 #include <assert.h>
 #include <stdlib.h>
 
@@ -36,12 +63,12 @@ napi_value get_constructor(napi_env env) {
   return cons;
 }
 
-/// @brief 获取 `Value` 类型对象的 `value` 属性值
+/// @brief 获取 Node 中 `Value` 类型对象的 `value` 属性值
 ///
 /// 当在 Node 中通过 `Value` 类对象获取 `value` 属性值时, 会实际调用该方法, 具体调用流程如下:
 /// 1. 调用该方法时, Node 会通过 `napi_callback_info` 类型参数传递其对象的 `this` 引用值;
 /// 2. 通过 `napi_unwrap` 函数, 可以将 Node 的 `this` 引用值转为实际的 `Value` 结构体指针值;
-/// 3. 通过 `Value` 结构体指针获取其 `value` 字段值, 并通过该值创建对应的 `napi_value` 值并返回;
+/// 3. 通过 `Value` 结构体指针获取其 `value` 字段值, 并通过该值创建对应的 `napi_value` 类型返回值;
 ///
 /// @param env 上下文对象
 /// @param info 函数调用信息
@@ -49,13 +76,13 @@ napi_value get_constructor(napi_env env) {
 napi_value get_value(napi_env env, napi_callback_info info) {
   napi_value this_;
 
-  // 从上下文对象中获取调用当前函数时的 Node 对应 `this` 引用对象以及调用函数的 Node 参数
+  // 从上下文对象中获取调用当前函数时的 Node 对应 `this` 引用以及调用函数的 Node 参数
   napi_status status = napi_get_cb_info(env, info, NULL, NULL, &this_, NULL);
   assert(status == napi_ok);
 
   Value* obj;
 
-  // 将 Node 对应的 `this` 引用对象解开为 `Value` 结构体变量指针
+  // 将 Node 对应的 `this` 引用解开为 `Value` 结构体变量指针
   status = napi_unwrap(env, this_, (void**)&obj);
   assert(status == napi_ok);
 
@@ -68,79 +95,133 @@ napi_value get_value(napi_env env, napi_callback_info info) {
   return num;
 }
 
+/// @brief 为 Node 中 `Value` 类型对象设置 `value` 属性值
+///
+/// 当在 Node 中通过 `Value` 类对象设置 `value` 属性值时, 会实际调用该方法, 具体调用流程如下:
+/// 1. 调用该方法时, Node 会通过 `napi_callback_info` 类型参数传递要设置的 `value` 属性值, 为 `napi_value` 类型;
+/// 2. 同样通过 `napi_callback_info` 类型参数传递其对象的 `this` 引用值;
+/// 3. 通过 `napi_unwrap` 函数, 可以将 Node 的 `this` 引用值转为实际的 `Value` 结构体指针值;
+/// 4. 将参数传递的 `value` 属性值设置到结构体中, 完成值的设置;
+///
+/// @param env 上下文对象
+/// @param info 函数调用信息
+/// @return `NULL` 值表示不像 Node 返回值
 napi_value set_value(napi_env env, napi_callback_info info) {
   size_t argc = 1;
   napi_value args[1];
+
   napi_value this_;
 
+  // 获取 Node 调用此函数传递的参数以及 `this` 引用
   napi_status status = napi_get_cb_info(env, info, &argc, args, &this_, NULL);
   assert(status == napi_ok);
 
   Value* obj;
+
+  // 将 `this` 引用解开为 `Value` 结构体指针
   status = napi_unwrap(env, this_, (void**)&obj);
   assert(status == napi_ok);
 
+  // 将参数传递的值设置到结构体 `value` 字段上
   status = napi_get_value_double(env, args[0], &obj->value);
   assert(status == napi_ok);
 
   return NULL;
 }
 
+/// @brief 为 Node 中 `Value` 类型实现 `plusOne` 方法
+///
+/// 该函数将 `Value` 类型的 `value` 属性值 `+1` 后返回相加后的结果
+///
+/// @param env 上下文对象
+/// @param info 函数调用信息
+/// @return `value` 属性 `+1` 后的结果
 napi_value plus_one(napi_env env, napi_callback_info info) {
   napi_value this_;
 
+  // 获取 Node 调用此函数传递的参数以及 `this` 引用
   napi_status status = napi_get_cb_info(env, info, NULL, NULL, &this_, NULL);
   assert(status == napi_ok);
 
   Value* obj;
+
+  // 将 `this` 引用解开为 `Value` 结构体指针
   status = napi_unwrap(env, this_, (void**)&obj);
   assert(status == napi_ok);
 
+  // 将 `Value` 结构体的 `value` 字段值 `+1`
   obj->value += 1;
 
   napi_value num;
+
+  // 将 `Value` 结构体的 `value` 字段值转换为 `napi_value` 类型后返回
   status = napi_create_double(env, obj->value, &num);
   assert(status == napi_ok);
 
   return num;
 }
 
+/// @brief 为 Node 中 `Value` 类型实现 `multiply` 方法
+///
+/// 该函数将 `Value` 类型的 `value` 属性值乘以指定值后, 返回新的 `Value` 对象
+///
+/// @param env 上下文对象
+/// @param info 函数调用信息
+/// @return 新的 Node `Value` 对象, 其 `value` 属性为当前对象的 `value` 属性值乘以 `n` 的结果
 napi_value multiply(napi_env env, napi_callback_info info) {
-  napi_value this_;
-
   size_t argc = 1;
   napi_value args[1];
+
+  napi_value this_;
+
+  // 获取 Node 调用此函数传递的参数以及 `this` 引用
   napi_status status = napi_get_cb_info(env, info, &argc, args, &this_, NULL);
   assert(status == napi_ok);
 
   napi_valuetype v_type;
+
+  // 获取参数类型
   status = napi_typeof(env, args[0], &v_type);
   assert(status == napi_ok);
 
   double multiple = 1;
   if (v_type != napi_undefined) {
+    // 获取参数值
     status = napi_get_value_double(env, args[0], &multiple);
     assert(status == napi_ok);
   }
 
   Value* obj;
+
+  // 将 `this` 引用解开为 `Value` 结构体指针
   status = napi_unwrap(env, this_, (void**)&obj);
   assert(status == napi_ok);
 
   napi_value argv[1];
+
+  // 将结构体 `value` 字段乘以 `n` 的结果转换为 `napi_value` 类型, 存入 `argv[0]` 中
   status = napi_create_double(env, obj->value * multiple, argv);
   assert(status == napi_ok);
 
   napi_value instance;
+
+  // 调用上下文中绑定的 Node 构造器, 并传递参数, 产生一个 Node 中 `Value` 类型对象
   status = napi_new_instance(env, get_constructor(env), 1, argv, &instance);
   assert(status == napi_ok);
 
   return instance;
 }
 
+/// @brief 销毁 Node 对象
+///
+/// @param env
+/// @param ptr
+/// @param
 void destroy_object(napi_env env, void* ptr, void* /*finalize_hint*/) {
   Value* obj = (Value*)ptr;
+
   // 销毁 obj 指针指向的对象
+  free(obj);
 }
 
 /// @brief 创建新的 Node 对象
@@ -210,13 +291,13 @@ napi_value new_object(napi_env env, napi_callback_info info) {
 
   // 如果在 Node 中直接调用 `Value` 函数 (即 `Value(...)` 而非 `new Value(...)`)
 
-  // 获取调用函数时传递的参数
+  // 获取调用函数时传递的参数, 此时无法获取 `this` 引用
   status = napi_get_cb_info(env, info, &argc, args, NULL, NULL);
   assert(status == napi_ok);
 
   napi_value instance;
 
-  // 通过绑定的 Node 构造函数, 创建 `Value` 类型的实例
+  // 通过绑定的 Node 构造函数, 创建 `Value` 类型的实例, 调用构造函数时又会调用 `new_object` 函数, 并令 `new.target` 不为 `NULL` 值
   status = napi_new_instance(env, get_constructor(env), argc, args, &instance);
   assert(status == napi_ok);
 
