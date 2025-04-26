4469110f7231a9e8d7982f36ce1592689217646e
Alvin <quhao317@163.com>
2025-04-26 22:20:22 +0800

    update eslint config file

--- toolkit/yarn/module/dist/lib/utils.js
+++
@@ -1,5 +0,0 @@
-export async function version() {
-    const conf = await import('../../package.json', { with: { type: 'json' } });
-    return `${conf.default['name']}@${conf.default['version']}`;
-}
-//# sourceMappingURL=utils.js.map
