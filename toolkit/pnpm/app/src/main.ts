54b9b050d426da646de3d4f2c85e870f801000ba
Alvin <quhao317@163.com>
2025-09-22 01:54:00 +0800

    [toolkit/pnpm] update package.json file

---
+++ toolkit/pnpm/app/src/main.ts
@@ -0,0 +1,11 @@
+import { version as lVersion } from '@toolkit/pnpm-lib';
+import { version as wVersion } from '@toolkit/pnpm-app-lib';
+
+/**
+ * 程序入口函数
+ */
+export async function main(): Promise<void> {
+  const lVer = await lVersion();
+  const wVer = await wVersion();
+  console.log(`Hello PNPM!, repo lib version is: ${lVer}, workspace lib version is: ${wVer}`);
+}
